4282fe191edd29d238af22a15e8af420cf7469a4
Geovani Ferreira <geovani.utfpr@gmail.com>
2024-02-17 15:47:55 -0300

    fix bug of projectile, add sound effect. Created enemy scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 500.0
 var projectile_scene = preload("res://Scenes/projectile.tscn")
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var timer = $shoot_coldown
 
 func _physics_process(delta):
 	velocity = Vector2(0,0)
@@ -18,8 +16,6 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 		
 	var screen_size = get_viewport_rect().size
-#	position.x = clamp(position.x, 50, screen_size.x-50)
-#	position.y = clamp(position.y, 50, screen_size.y-50)
 	position = position.clamp(Vector2(50,50), screen_size - Vector2(50,50))
 	move_and_slide()
 
@@ -29,8 +25,10 @@ func _process(delta):
 		shoot()
 	
 func shoot():
-	print('shoot')
-	var projectile = projectile_scene.instantiate()
-#	projectile.position = position
-#	projectile.initial_rotation = -90
-	add_child(projectile)
+	if timer.is_stopped():
+		timer.start()
+		var projectile = projectile_scene.instantiate()
+		var shoot_position = position
+		projectile.global_position = shoot_position
+		$projectile_box.add_child(projectile)
+		
--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -2,12 +2,16 @@ extends Area2D
 
 var speed = 400
 var direction = Vector2(1,0)
-#var global_initial_rotation = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	visible = true
+	var sound = $LaserSound
+	sound.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position += direction * speed * delta
 	global_rotation = 0
+
+
+func _on_visible_notifier_screen_exited():
+	queue_free()
